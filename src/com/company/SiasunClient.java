aafcd58a8d3a949c5e8000b6ea66725236448fe5
QYFQ <1401156900@qq.com>
2020-04-03 17:52:45 +0800

    完善新松机器人的相关方法

--- src/com/company/SiasunClient.java
+++ src/com/company/SiasunClient.java
@@ -26,27 +26,58 @@ public class SiasunClient implements Runnable {
                 System.arraycopy(buffer,0,bytes,0,len);
                 ByteUtils.printHexString("",bytes);
                 int code=ByteUtils.bytes2Int(bytes,5);
-                if (code==0x0205){
-                    byte[] response=new byte[44];
-                    for (int i=0;i<6;i++){
-                        System.arraycopy(ByteUtils.float2Bytes((float)(i*0.5)),0,response,20+i*4,4);
-                    }
-                    outputStream.write(response);
-                    outputStream.flush();
-                }
-                if (code==0x0302){
-                    byte[] response=new byte[71];
-                    System.arraycopy(ByteUtils.short2Bytes((short)0x1234),0,response,20,2);
-                    outputStream.write(response);
-                    outputStream.flush();
-                }
-                if (code==0x0307){
-                    byte[] response=new byte[21];
-                    byte[] state=new byte[1];
-                    state[0]=(byte)0x01;
-                    System.arraycopy(state,0,response,20,1);
-                    outputStream.write(response);
-                    outputStream.flush();
+                byte[] response;
+                switch (code){
+                    case 0x0205:
+                        response=new byte[44];
+                        for (int i=0;i<6;i++){
+                            System.arraycopy(ByteUtils.float2Bytes((float)(i*0.5)),0,response,20+i*4,4);
+                        }
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    case 0x0302:
+                        response=new byte[71];
+                        System.arraycopy(ByteUtils.short2Bytes((short)0x1234),0,response,20,2);
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    case 0x0307:
+                        response=new byte[21];
+                        byte[] state=new byte[1];
+                        state[0]=(byte)0x01;
+                        System.arraycopy(state,0,response,20,1);
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    case 0x0202:
+                        response=new byte[56];
+                        for (int i=0;i<9;i++){
+                            System.arraycopy(ByteUtils.float2Bytes((float)(i*0.1)),0,response,20+i*4,4);
+                        }
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    case 0x1201:
+                        response=new byte[56];
+                        for (int i=0;i<9;i++){
+                            System.arraycopy(ByteUtils.float2Bytes((float)(i*0.05)),0,response,20+i*4,4);
+                        }
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    case 0x1200:
+                        response=new byte[56];
+                        for (int i=0;i<9;i++){
+                            System.arraycopy(ByteUtils.float2Bytes((float)(i*1.5)),0,response,20+i*4,4);
+                        }
+                        outputStream.write(response);
+                        outputStream.flush();
+                        break;
+                    default:
+                        response=new byte[20];
+                        outputStream.write(response);
+                        outputStream.flush();
                 }
             }
         }catch (Exception e){
